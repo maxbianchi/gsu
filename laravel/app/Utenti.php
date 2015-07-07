5dccd173e4a0af11d50c66fa36d01e8ba56ca985
max <max.bianchi81@gmail.com>
2015-07-07 14:32:34 +0200

    modifica codice utente

--- laravel/app/Utenti.php
+++ laravel/app/Utenti.php
@@ -20,7 +20,7 @@ class Utenti extends Model {
         $pwd = Input::get("password");
 
         if($usr == "uniweb")
-            $usr = "00375";
+            $usr = "ws00375";
 
         $where = ['UTENTE' => $usr, 'PASSWORD' => $pwd];
         $res = $this::where($where)->get()->toArray();
