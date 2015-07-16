7fc95299383c6b341195c23cb8be2fa6f696e9e4
max <max.bianchi81@gmail.com>
2015-07-16 16:03:41 +0200

    aggiunta immagine al login

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -5,45 +5,53 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Benvenuto nel Nuovo Portale Uniweb 4.0, la tua area riservata!</div>
-				<div class="panel-body">
-                <div><b><a href="{{ url('/registrazione') }}">Se non sei ancora registrato clicca qui</a></b></div>
-                                @if (count(Session::get('errors')) > 0)
-                                    <div class="alert alert-danger">
-                                        {{ Session::get('errors') }}
-                                    </div>
-                                @endif
+
+    <div class="row">
+        <div class="col-md-7 col-md-offset-3">
+            <img src="{{ URL::asset('images/areariservata_login.jpg') }}" alt="Uniweb 4.0 Dashboard" title="Uniweb 4.0 Dashboard" style="width:84%; padding-left:10px;">
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Benvenuto nel Nuovo Portale Uniweb 4.0, la tua area riservata!</div>
+                    <div class="panel-body">
+                        <div><b><a href="{{ url('/registrazione') }}" style="text-decoration: underline;">Se non sei ancora registrato clicca qui</a></b></div>
+                        @if (count(Session::get('errors')) > 0)
+                            <div class="alert alert-danger">
+                                {{ Session::get('errors') }}
+                            </div>
+                        @endif
 
 
-					<form method="POST" action="{{ url('/login') }}" class="form-2">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <form method="POST" action="{{ url('/login') }}" class="form-2">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                        <h1><span class="sign-up">Log in</span> </h1>
-                        <p class="float">
-                            <label for="login"><i class="icon-user"></i>Username</label>
-                            <input type="text" name="username" placeholder="Username or email" required>
-                        </p>
-                        <p class="float">
-                            <label for="password"><i class="icon-lock"></i>Password</label>
-                            <input type="password" name="password" placeholder="Password" class="showpassword" required>
-                        </p>
-                        <div class="center">
+                            <h1><span class="sign-up">Log in</span> </h1>
+                            <p class="float">
+                                <label for="login"><i class="icon-user"></i>Username</label>
+                                <input type="text" name="username" placeholder="Username or email" required>
+                            </p>
+                            <p class="float">
+                                <label for="password"><i class="icon-lock"></i>Password</label>
+                                <input type="password" name="password" placeholder="Password" class="showpassword" required>
+                            </p>
+                            <div class="center">
 
-                            <input type="submit" name="submit" value="Log in">
-                        </div>
+                                <input type="submit" name="submit" value="Log in">
+                            </div>
 
-                        <p style="padding-top:10px;"><a href="{{url('/password')}}" title="Recupera la tua password di accesso">Hai dimenticato la tua password?</a></p>
-					</form>
+                            <p style="padding-top:10px;"><a href="{{url('/password')}}" title="Recupera la tua password di accesso">Hai dimenticato la tua password?</a></p>
+                        </form>
 
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 
