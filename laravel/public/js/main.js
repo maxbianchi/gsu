c3d0502f2a6143e575ffde1e6013fbab7b7ef685
max <max.bianchi81@gmail.com>
2015-07-21 14:29:18 +0200

    Fix su Excel

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -37,19 +37,6 @@ $(document).ready(function () {
         $('#delete').modal('show');
     });
 
-    $('table').each(function() {
-        var $table = $(this);
-
-        var $button = $("<button type='button'>");
-        $button.text("Export to spreadsheet");
-        $button.insertAfter($table);
-
-        $button.click(function() {
-            var csv = $table.table2CSV({delivery:'value'});
-            window.location.href = 'data:text/csv;charset=UTF-8,'
-            + encodeURIComponent(csv);
-        });
-    });
 
     $("body").on("click", ".exportCSV", function(event) {
         console.log("QUI");
@@ -63,7 +50,7 @@ $(document).ready(function () {
         outputFile = outputFile.replace('.csv','') + '.csv'
 
         // CSV
-        exportTableToCSV.apply(this, [$('#dvData>table'), outputFile]);
+        exportTableToCSV.apply(this, [$('#main'), outputFile]);
 
     });
 
