bce55debc91d895554576c2af1e160f851792016
max <max.bianchi81@gmail.com>
2015-09-07 17:07:58 +0200

    bug fix unigate

--- laravel/app/modules/gsu/Models/UnigateModel.php
+++ laravel/app/modules/gsu/Models/UnigateModel.php
@@ -64,7 +64,7 @@ class UnigateModel extends Model {
 			UNIGATE.WANSUB,
 			UNIGATE.ELIMINATO
 			FROM gsu.dbo.UNIGATE
-			RIGHT OUTER JOIN UNIWEB.dbo.AOF70 richieste ON UNIGATE.codice_r = richieste.MANUTENZIONE
+			LEFT OUTER JOIN UNIWEB.dbo.AOF70 richieste ON UNIGATE.codice_r = richieste.MANUTENZIONE
 			LEFT OUTER JOIN	UNIWEB.dbo.AGE10	anagrafica1	ON richieste.SOGGETTO				= anagrafica1.SOGGETTO
 			LEFT OUTER JOIN	UNIWEB.dbo.AGE10	anagrafica2	ON richieste.CLIENTE				= anagrafica2.SOGGETTO
 			LEFT OUTER JOIN	UNIWEB.dbo.AGE10	anagrafica3	ON richieste.DESTINATARIOABITUALE	= anagrafica3.SOGGETTO
@@ -151,7 +151,7 @@ EOF;
 			UNIGATE.WANSUB,
 			UNIGATE.ELIMINATO
 			FROM gsu.dbo.UNIGATE
-			RIGHT OUTER JOIN UNIWEB.dbo.AOF70 richieste ON UNIGATE.codice_r = richieste.MANUTENZIONE
+			LEFT OUTER JOIN UNIWEB.dbo.AOF70 richieste ON UNIGATE.codice_r = richieste.MANUTENZIONE
 			LEFT OUTER JOIN	UNIWEB.dbo.AGE10	anagrafica1	ON richieste.SOGGETTO				= anagrafica1.SOGGETTO
 			LEFT OUTER JOIN	UNIWEB.dbo.AGE10	anagrafica2	ON richieste.CLIENTE				= anagrafica2.SOGGETTO
 			LEFT OUTER JOIN	UNIWEB.dbo.AGE10	anagrafica3	ON richieste.DESTINATARIOABITUALE	= anagrafica3.SOGGETTO
