e5780d9a580288ce454663f1639c3ffd04ae71de
max <max.bianchi81@gmail.com>
2015-11-16 16:19:36 +0100

    apertura ticket lato cliente

--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => 'staff@uniweb.it', 'name' => 'staff@uniweb.it'],
+	'from' => ['address' => 'staff@uniweb.it', 'name' => 'TICKET UNIWEB'],
 
 	/*
 	|--------------------------------------------------------------------------
